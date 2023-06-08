08797354e86835a87644012863e121c313778ae5
Sydney Alberson <sydney.alberson@gmail.com>
2023-06-08 14:17:32 -0400

    Added color range to temperature

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const state = {
-    tempNumber: 52 //temporary num, make default current temp
+    tempNumber: 70 //temporary num, make default current temp
 }
 
 const initialTemp = () => {
     const temp = document.getElementById("temp-number")
     temp.textContent = state.tempNumber;
+    changeColorTemp()
 }
 
 const decreaseTemp = () => {
     const temp = document.getElementById("temp-number")
     temp.textContent = state.tempNumber--;
+    changeColorTemp()
 }
 
 const increaseTemp = () => {
     const temp = document.getElementById("temp-number")
     temp.textContent = state.tempNumber++;
+    changeColorTemp()
 }
+
+const changeColorTemp = () => {
+    
+    const temp = document.querySelector("#temp-number")
+    // const currentColor = temp.classList
+    
+    if (state.tempNumber >= 80) {
+        temp.classList = ["red"]
+        console.log("red")
+    } else if (state.tempNumber >= 70 && state.tempNumber <= 79) {
+        temp.classList = ["orange"]
+        console.log("orange")
+    } else if (state.tempNumber >= 60 && state.tempNumber <= 69) {
+        temp.classList = ["yellow"]
+        console.log("yellow")
+    } else if (state.tempNumber >= 50 && state.tempNumber <= 59) {
+        temp.classList = ["green"]
+    } else if (state.tempNumber <= 49) {
+        temp.classList = ["teal"]
+    }
+}
+
+
 
 const registerEvents = () => {
     initialTemp()
@@ -26,7 +52,6 @@ const registerEvents = () => {
 }
 
 document.addEventListener("DOMContentLoaded", registerEvents)
-
 
 
 
